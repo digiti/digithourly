aa8c5e16217be74a761968f32d3312c1313c0320
Hoetmaaiers <houdmeyers@gmail.com>
2014-11-08 00:46:04 +0100

    toggle between hours & minutes

--- javascripts/digithourly.js
+++ javascripts/digithourly.js
@@ -1,6 +1,6 @@
 var DigitEntry = function(element) {
   this.element = element;
-  console.log("new DigitEntry");
+  this.unit = 'hours';
   
   this.template = '<input type="text" value=0 tabindex=1 size=5 />'
                 + '<a class="more" onclick="return false;"></a>'
@@ -13,7 +13,7 @@ DigitEntry.prototype = (function() {
   var setupCustomInput = function(){
     // hide hours entry
     this.originalEntry = this.element.querySelector('.customhours');
-    this.originalEntry.style.display = 'none';
+    // this.originalEntry.style.display = 'none';
 
     // create and insert polyfunctional entry    
     this.newEntry = document.createElement('div');
@@ -27,16 +27,33 @@ DigitEntry.prototype = (function() {
     this.newInput.value = this.originalInput.value * 60;
   };
 
+  var toggleUnit = function() {
+    var label = this.newEntry.querySelector('p');
+    var input = this.newEntry.querySelector('input');
+
+    if (this.unit == 'minutes') {
+      this.unit = 'hours';
+      label.innerHTML = "Hours";
+      input.value = input.value / 60;
+    } else {
+      this.unit = 'minutes';
+      label.innerHTML = "Minutes";
+      input.value = input.value * 60;
+    }
+  }
+
   var updateOriginalInput = function() {
     var value = this.newInput.value;
     setOriginalInput.bind(this)(value);
   }
 
   var setOriginalInput = function(value) {
-    this.originalInput.value = value / 60;
-    console.log(this.originalInput.value);
+    if (this.unit == "hours") {
+      this.originalInput.value = value;
+    } else {
+      this.originalInput.value = value / 60;
+    }
   }
-
 
   var setupActions = function() {
     
@@ -49,15 +66,26 @@ DigitEntry.prototype = (function() {
 
     var moreButton = this.newEntry.querySelector('.more');
     moreButton.addEventListener('click', increase.bind(this));
+
+    var label = this.newEntry.querySelector('p');
+    label.addEventListener('click', toggleUnit.bind(this));
   }
   
+  var operationPart = function() {
+    if (this.unit == "minutes") {
+      return 15;
+    } else {
+      return 0.25;
+    }
+  }
+
   var decrease = function() {
-    this.newInput.value = (this.newInput.value * 1) - 15;
+    this.newInput.value = (this.newInput.value * 1) - operationPart.bind(this)();
     updateOriginalInput.bind(this)();
   }
 
   var increase = function() {
-    this.newInput.value = (this.newInput.value * 1) + 15;
+    this.newInput.value = (this.newInput.value * 1) + operationPart.bind(this)();
     updateOriginalInput.bind(this)();
   }
 
